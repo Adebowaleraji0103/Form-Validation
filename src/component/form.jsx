398dc50f73bde0ed40ca923308e5a56ff15d6a6f
Mubarak <adebowaleraji0103@gmail.com>
2025-12-04 14:38:39 +0100

    d

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,18 +1,44 @@
 import Input from "./input";
 import Button from "./button";
+import { useState } from "react";
 
 const formInputs = [
-  { type: "text", holder: "First Name" },
-  { type: "text", holder: "Last Name" },
-  { type: "email", holder: "Email Address" },
-  { type: "password", holder: "Password" },
+  { type: "text", holder: "First Name", name: "firstname" },
+  { type: "text", holder: "Last Name", name: "lastname" },
+  { type: "email", holder: "Email Address", name: "email" },
+  { type: "password", holder: "Password", name: "password" },
 ];
 
 export default function Form() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
   return (
-    <form>
+    <form
+      method="post"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(formData);
+      }}
+    >
       {formInputs.map((input, id) => (
-        <Input type={input.type} placeholder={input.holder} key={id} />
+        <Input
+          type={input.type}
+          placeholder={input.holder}
+          key={id}
+          name={input.name}
+          value={formData[input.name]}
+          onChange={handleChange}
+        />
       ))}
       <Button> Claim your free trial </Button>
       <p>
